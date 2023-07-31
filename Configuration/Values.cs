86a3f58af4882c394f5f54f0adc0aaa9142ee443
rdheensa <dheensa@sky.com>
2023-07-31 17:56:30 +0100

    initial commit

--- Configuration/Values.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WeatherApp.Configuration
-{
-    public class Values
-    {
-        public const string OPEN_WEATHER_APP_ID = "d80e29f1065645f82c11bbaebf57af65";
-    }
-}
--- OpenWeatherMap Models/Coord.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WeatherApp.OpenWeatherMap_Models
-{
-    public class Coord
-    {
-        public float Lon { get; set; }
-        public float Lat { get; set; }
-    }
-}
